a56099cdfd0bad4b64e8b22289b5f168b285e2b0
Federico-Maiocchi <maiocchifederico@gmail.com>
2024-04-16 16:33:19 +0200

    ADD: ex array

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            //-Inizializzazione array cibi preferiti
+            string[] cibiPreferiti = { 
+                "Spaghetti con le cozze", 
+                "Pizza", 
+                "Sushi", 
+                "Fegato alla veneziana",
+                "Ostriche"
+            };
+
+            //-La lunghezza della classifica
+            int cibiPrefereritiLunghezza = cibiPreferiti.Length;
+            Console.WriteLine(cibiPrefereritiLunghezza);
+
+            //-La vostra classifica(dunque stampare l’intero array in ordine indicando la posizione in classifica)
+            for (int i = 0; i < cibiPreferiti.Length; i++)
+            {
+                Console.WriteLine(cibiPreferiti[i]);
+            }
+
+            //-Il vostro cibo top(prima posizione della classifica)
+            Console.WriteLine(cibiPreferiti[0]);
+
+            //-Il vostro cibo preferito ma non troppo(ultima posizione della classifica)
+            Console.WriteLine(cibiPreferiti[4]);
         }
     }
 }
